527de4f8a8249a4739c515a350294ca3d991d142
Fanny1212 <fannyumutoniwase086@gmail.com>
2026-02-06 00:41:35 +0200

    Initial commit: Lab set up

---
+++ main/PartOne/Q1.dart
@@ -0,0 +1,18 @@
+/*
+
+Q1: Write a Dart function named `welcomeMessage` that prints a welcome message for the 
+school system. 
+
+*/
+
+void welcomeMessage() {
+
+  print("Welcome to the School Management System!");
+  
+}
+
+void main() {
+
+  welcomeMessage();
+
+}
